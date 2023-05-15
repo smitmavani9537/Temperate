66b24abbe84c93cada5bf4e3080af5858e7cfa26
smitmavani9537 <131868368+smitmavani9537@users.noreply.github.com>
2023-05-15 16:05:39 +0530

    Add files via upload

---
+++ P3T1.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+    clrscr();
+    char ch='A';
+    do{
+       printf("%c\t",ch);
+       ch++;
+      }while(ch<='Z');
+    getch();
+}
---
+++ P3T8.C
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+    clrscr();
+    int n,i,num=0;
+    printf("Enter a Number :");
+    scanf("%d",&n);
+
+    if (n==0 && n==1)
+    {
+     printf("%d is not a prise number.",n);
+    }
+    else
+       {
+     for (i=2;i<=n/2;i++)
+       {
+	 if (n%i==0)
+	 {
+	    num=1;
+	break;
+	 }
+       }
+     if (num == 0)
+       {
+       printf("%d is a prise number.",n);
+       }
+     else
+      {
+      printf("%d is not a prise number.",n);
+      }
+    }
+    getch();
+}
